d3fe0a748b422c3582cf217fdf003af14326ec04
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-21 01:18:49 +0600

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const brands = require("./brands.json");
 const adds = require("./add.json");
-require("dotenv").config();
-const app = express();
+
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -24,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const carCollection = client.db("carDB").collection("cars");
 
@@ -99,7 +100,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
